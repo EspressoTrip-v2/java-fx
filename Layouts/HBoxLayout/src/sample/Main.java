55ae2f63d5fa8ec2352c8f45c9bedafaeda5811b
EspressoTrip-v2 <juanbo.jb@gmail.com>
2021-01-13 16:23:36 +0200

    Layouts

--- Layouts/HBoxLayout/src/sample/Main.java
+++ Layouts/HBoxLayout/src/sample/Main.java
@@ -9,9 +9,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("HBox");
         primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
     }
